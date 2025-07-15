65011f99c5d720c3e0620ffd61b28ddd13785ef9
Jacob Templeton <jacob.templeton@gmail.com>
2025-07-15 00:30:49 -0700

    Add update to vercel url

--- src/server.ts
+++ src/server.ts
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://your-dnd-app.vercel.app",
+    origin: "https://dnd-character-creator-iota.vercel.app",
     credentials: true,
   })
 );
